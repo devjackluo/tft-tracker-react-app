c3c58ae5f112002d8c9280a97e710558cd3dc68c
Jack Luo <jackluo94@hotmail.com>
2019-10-12 20:18:31 -0700

    Updates

---
+++ precache-manifest.d5ce42b26c60826bca3b0a5f528ab8b5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bbeb1fb06099d37433b9d97d165b562",
+    "url": "/tft-tracker-react-app/index.html"
+  },
+  {
+    "revision": "cf182e88e484824db679",
+    "url": "/tft-tracker-react-app/static/css/main.2a89d21e.chunk.css"
+  },
+  {
+    "revision": "6baeea1694fb64b81d3c",
+    "url": "/tft-tracker-react-app/static/js/2.ec35f0bc.chunk.js"
+  },
+  {
+    "revision": "cf182e88e484824db679",
+    "url": "/tft-tracker-react-app/static/js/main.207d4ce6.chunk.js"
+  },
+  {
+    "revision": "456d0761c7a9429ac5a4",
+    "url": "/tft-tracker-react-app/static/js/runtime-main.5244b84e.js"
+  }
+]);
